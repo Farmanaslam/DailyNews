6c6514e5d67a9e777ed54f21d92ead43703f8076
farman <farmanaslam674@gmail.com>
2024-01-12 20:08:37 +0530

    update togglemode checkbox

--- src/App.js
+++ src/App.js
@@ -1,59 +1,140 @@
+import "./App.css";
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import LoadingBar from "react-top-loading-bar";
 
-import './App.css';
-import React, { useState } from 'react'
-import Navbar from './components/Navbar';
-import News from './components/News';
-import LoadingBar from 'react-top-loading-bar';
-
-import {
-  BrowserRouter as Router,
- Routes,
-  Route,
-  
-} from "react-router-dom";
-const  App =()=> {
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+const App = () => {
   const [progress, setprogress] = useState(0);
-  const [mode,setMode]=useState(`info`);
-
+  const [mode, setMode] = useState(`light`);
 
   // state={
   //   progress:0
   // }
-  
- 
+  const toggleMode = () => {
+    if (mode === `light`) {
+      setMode(`dark`);
+      document.body.style.backgroundColor = "#242939";
+      document.body.style.color = "white";
+    } else {
+      setMode(`light`);
+      document.body.style.backgroundColor = "white";
 
-  
-    return (
+      document.body.style.color = "black";
+    }
+  };
 
-      <div>
-        <Router>
-        <Navbar mode={mode} />
+  return (
+    <div>
+      <Router>
+        <Navbar mode={mode} toggleMode={toggleMode} />
         {/* <h1>First Class Component {this.c}</h1> */}
-        <LoadingBar
-        height={3}
-        color='#f11946'
-        progress={progress}
-      />
+        <LoadingBar height={3} color="#f11946" progress={progress} />
 
-       
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <News
+                setProgress={setprogress}
+                apiKey={"fbf9271dc1784cb5b65e522170277b16"}
+                key={"general"}
+                pageSize={6}
+                country="in"
+                category="general"
+              />
+            }
+          ></Route>
+          <Route
+            exact
+            path="/sports"
+            element={
+              <News
+                setProgress={setprogress}
+                apiKey={"fbf9271dc1784cb5b65e522170277b16"}
+                key={"sports"}
+                pageSize={6}
+                country="in"
+                category="sports"
+              />
+            }
+          ></Route>
 
-<Routes>
-<Route exact path="/" element={<News setProgress={setprogress}  apiKey={"fbf9271dc1784cb5b65e522170277b16"} key={"general"} pageSize={6} country="in" category="general"/>}></Route>
-<Route exact path="/sports" element={<News setProgress={setprogress}   apiKey={"fbf9271dc1784cb5b65e522170277b16"} key={"sports"} pageSize={6} country="in" category="sports"/> }></Route>
-
-<Route exact path="/business" element={<News setProgress={setprogress}   apiKey={"fbf9271dc1784cb5b65e522170277b16"} key={"business"} pageSize={6} country="in" category="business"/> }></Route>
-<Route exact path="/entertainment" element={<News setProgress={setprogress}  apiKey={"fbf9271dc1784cb5b65e522170277b16"}  key={"entertainment"} pageSize={6} country="in" category="entertainment"/> }></Route>
-<Route exact path="/health" element={<News setProgress={setprogress}  apiKey={"fbf9271dc1784cb5b65e522170277b16"}  key={"health"} pageSize={6} country="in" category="health"/> }></Route>
-<Route exact path="/science" element={<News setProgress={setprogress}  apiKey={"fbf9271dc1784cb5b65e522170277b16"}  key={"science"} pageSize={6} country="in" category="science"/> }></Route>
-<Route exact path="/technology" element={<News setProgress={setprogress}  apiKey={"fbf9271dc1784cb5b65e522170277b16"}  key={"technology"} pageSize={6} country="in" category="technology"/> }></Route>
-
-</Routes>
-
-       </Router>
-
-      </div>
-    )
-  }
-export default App
-
+          <Route
+            exact
+            path="/business"
+            element={
+              <News
+                setProgress={setprogress}
+                apiKey={"fbf9271dc1784cb5b65e522170277b16"}
+                key={"business"}
+                pageSize={6}
+                country="in"
+                category="business"
+              />
+            }
+          ></Route>
+          <Route
+            exact
+            path="/entertainment"
+            element={
+              <News
+                setProgress={setprogress}
+                apiKey={"fbf9271dc1784cb5b65e522170277b16"}
+                key={"entertainment"}
+                pageSize={6}
+                country="in"
+                category="entertainment"
+              />
+            }
+          ></Route>
+          <Route
+            exact
+            path="/health"
+            element={
+              <News
+                setProgress={setprogress}
+                apiKey={"fbf9271dc1784cb5b65e522170277b16"}
+                key={"health"}
+                pageSize={6}
+                country="in"
+                category="health"
+              />
+            }
+          ></Route>
+          <Route
+            exact
+            path="/science"
+            element={
+              <News
+                setProgress={setprogress}
+                apiKey={"fbf9271dc1784cb5b65e522170277b16"}
+                key={"science"}
+                pageSize={6}
+                country="in"
+                category="science"
+              />
+            }
+          ></Route>
+          <Route
+            exact
+            path="/technology"
+            element={
+              <News
+                setProgress={setprogress}
+                apiKey={"fbf9271dc1784cb5b65e522170277b16"}
+                key={"technology"}
+                pageSize={6}
+                country="in"
+                category="technology"
+              />
+            }
+          ></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+};
+export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,73 +1,104 @@
-import React from 'react'
-import { useState } from 'react';
-import {Link} from "react-router-dom"
- const Navbar=(props)=>  {
-  const [mode,setMode]=useState(`info`);
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+const Navbar = (props) => {
   
 
+  // const toggleMode = () => {
+  //   if (mode === `info`) {
+  //     setMode(`dark`);
+  //     document.body.style.backgroundColor = "#242939";
+  //     document.body.style.color = "white";
+  //   } else {
+  //     setMode(`info`);
+  //     document.body.style.backgroundColor = "white";
 
-  const toggleMode=()=>{
-  
-    if(mode===`info`){
-  setMode(`dark`)
-  document.body.style.backgroundColor="#242939";
- 
-document.body.style.color="white";
-    }else{
-      setMode(`info`)
-      document.body.style.backgroundColor="white"
-    
-document.body.style.color="black";
-  
-    }
-  }
-  
-    return (
-      
-        <nav className={`navbar fixed-top navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
-        <div className="container-fluid">
-          <Link className="navbar-brand" to="#">DailyNews</Link>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-              </li>
-              
-              <li className="nav-item">
-                <Link className="nav-link" to="/sports">Sports</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/business">Business</Link>
-              </li>
-              
-              <li className="nav-item">
-                <Link className="nav-link" to="/entertainment">Entertainment</Link>
-              </li>
-            
-              <li className="nav-item">
-                <Link className="nav-link" to="/health">Health</Link>
-              </li>
-              
-              <li className="nav-item">
-                <Link className="nav-link" to="/science">Science</Link>
-              </li>
-              
-              <li className="nav-item">
-                <Link className="nav-link" to="/technology">Technology</Link>
-              </li>
-            </ul>
-            
-          </div>
-          <div id="id" className={`form-check form-switch text-${props.mode===`light`?`dark`:`light`}`}>
-  <input className="form-check-input" onClick={()=>{toggleMode()}} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
-  <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
-</div> 
+  //     document.body.style.color = "black";
+  //   }
+  // };
+
+  return (
+    <nav
+      className={`navbar fixed-top navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
+    >
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="#">
+          DailyNews
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/">
+                Home
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/sports">
+                Sports
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/business">
+                Business
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/entertainment">
+                Entertainment
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/health">
+                Health
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/science">
+                Science
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/technology">
+                Technology
+              </Link>
+            </li>
+          </ul>
         </div>
-      </nav>
-    )
-  
-}
+        <div
+          id="id"
+          className={`form-check form-switch text-${
+            props.mode === `light` ? `dark` : `light`
+          }`}
+        >
+          <input
+            className="form-check-input"
+            onClick={props.toggleMode}
+            type="checkbox"
+            role="switch"
+            id="flexSwitchCheckDefault"
+          />
+          <label className="form-check-label" htmlFor="flexSwitchCheckDefault">
+            Enable {props.mode==='light'?'Dark':'Light'} Mode
+          </label>
+        </div>
+      </div>
+    </nav>
+  );
+};
 export default Navbar;
